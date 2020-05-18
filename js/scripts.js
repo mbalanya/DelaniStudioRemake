910755d79213d19da0942e4414aa0d351487faf9
mbalanya <okobacharles@yahoo.co.uk>
2020-05-18 09:51:02 +0300

    add hover effect in the portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
   });
 });
 
+//what we do click toggles
 $(document).ready(function() {
   $('#designIcon').click(function(){
     $('#designIcon img').toggle();
@@ -42,5 +43,62 @@ $(document).ready(function() {
   $('#productManagementIcon').click(function(){
     $('#productManagementIcon img').toggle();
     $('#productManagementP').toggle();
+  });
+});
+
+//portfolio hover toggles
+$(document).ready(function() {
+  $('#blackProject').hover(function(){
+    $('#blackProject').toggle();
+    $('#blackProjectH2').toggle();
+  });
+});
+
+$(document).ready(function() {
+  $('#haveAGoodDayProject').hover(function(){
+    $('#haveAGoodDayProject').toggle();
+    $('#haveAGoodDayProjectH2').toggle();
+  });
+});
+
+$(document).ready(function() {
+  $('#mightyOntarioProject').hover(function(){
+    $('#mightyOntarioProject').toggle();
+    $('#mightyOntarioProjectH2').toggle();
+  });
+});
+
+$(document).ready(function() {
+  $('#buttonStylesProject').hover(function(){
+    $('#buttonStylesProject').toggle();
+    $('#buttonStylesProjectH2').toggle();
+  });
+});
+
+$(document).ready(function() {
+  $('#materialImageProject').hover(function(){
+    $('#materialImageProject').toggle();
+    $('#materialImageProjectH2').toggle();
+  });
+});
+
+$(document).ready(function() {
+  $('#burnedLogoProject').hover(function(){
+    $('#burnedLogoProject').toggle();
+    $('#burnedLogoProjectH2').toggle();
+  });
+});
+
+$(document).ready(function() {
+  $('#jimCarreyProject').hover(function(){
+    $('#jimCarreyProject').toggle();
+    $('#jimCarreyProjectH2').toggle();
+  });
+});
+
+$(document).ready(function() {
+  $('#baileyWonderProject').hover(function(){
+    $('#baileyWonderProject').toggle();
+    $('#baileyWonderProjectH2').toggle();
   });
 });
